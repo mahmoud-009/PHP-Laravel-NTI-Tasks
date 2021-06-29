825f1fed09359eeac23ecbdb3fa8a6864abcd8e4
Mahmoud Farahat <73858621+mahmoud-009@users.noreply.github.com>
2021-06-30 00:55:04 +0200

    new

--- task18/database/migrations/2021_06_29_224806_users.php
+++ task18/database/migrations/2021_06_29_224806_users.php
@@ -13,7 +13,12 @@ class Users extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('user', function (Blueprint $table) {
+        $table->id();
+        $table->string('name',100);
+        $table->string('email',100)->unique();
+        $table->string('password');
+    });
     }
 
     /**
